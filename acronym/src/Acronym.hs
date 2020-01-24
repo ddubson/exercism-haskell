824f544e052c2498f5ef82cd2eb848fd7d292b10
Dmitriy Dubson <d.dubson@gmail.com>
2020-01-24 17:58:04 -0500

    Continue: Acronym

--- acronym/src/Acronym.hs
+++ acronym/src/Acronym.hs
@@ -2,6 +2,11 @@ module Acronym (abbreviate) where
 
 import Data.Char
 
+allCaps :: [Char] -> Bool
+allCaps = not . all (isUpper)
+
+collectUpper :: [Char] -> [Char]
+collectUpper (x:xs) = (toUpper x) : (foldr (\e acc -> if isUpper e then e:acc else acc) [] xs)
+
 abbreviate :: String -> String
-abbreviate xs =  join . map . splitOn " " $ xs
---foldr (\x acc -> if isUpper x then x:acc else acc) [] xs
+abbreviate xs =  foldr (\x acc -> (collectUpper x) ++ acc) [] . filter allCaps $ words xs
