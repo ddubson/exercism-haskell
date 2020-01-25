eeda8abdc480e1ed5b532b561c67a1ef3b358ac5
Dmitriy Dubson <d.dubson@gmail.com>
2020-01-24 21:11:27 -0500

    Finish 1: Space Age

--- space-age/src/SpaceAge.hs
+++ space-age/src/SpaceAge.hs
@@ -10,4 +10,17 @@ data Planet = Mercury
             | Neptune
 
 ageOn :: Planet -> Float -> Float
-ageOn planet seconds = error "You need to implement this function."
+ageOn planet seconds = case planet of
+    Earth -> ageByOrbitalPeriodOffset $ 1
+    Mercury -> ageByOrbitalPeriodOffset $ 0.2408467
+    Venus -> ageByOrbitalPeriodOffset $ 0.61519726
+    Mars -> ageByOrbitalPeriodOffset $ 1.8808158
+    Jupiter -> ageByOrbitalPeriodOffset $ 11.862615
+    Saturn -> ageByOrbitalPeriodOffset $ 29.447498
+    Uranus -> ageByOrbitalPeriodOffset $ 84.016846
+    Neptune -> ageByOrbitalPeriodOffset $ 164.79132
+    _ -> 0.0
+    where
+        earthSecs = 31557600
+        ageByOrbitalPeriodOffset = (seconds /) . (*earthSecs)
+
