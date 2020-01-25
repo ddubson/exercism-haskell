6eacf784cc38df0ac90d7bdfbecd952976c67e8d
Dmitriy Dubson <d.dubson@gmail.com>
2020-01-24 19:30:53 -0500

    Finish 1: Acronym

--- acronym/src/Acronym.hs
+++ acronym/src/Acronym.hs
@@ -2,11 +2,14 @@ module Acronym (abbreviate) where
 
 import Data.Char
 
-allCaps :: [Char] -> Bool
-allCaps = not . all (isUpper)
+reduceIfAllCaps :: [Char] -> [Char]
+reduceIfAllCaps arr = if all isUpper $ arr then [head arr] else arr
 
 collectUpper :: [Char] -> [Char]
+collectUpper [] = []
 collectUpper (x:xs) = (toUpper x) : (foldr (\e acc -> if isUpper e then e:acc else acc) [] xs)
 
 abbreviate :: String -> String
-abbreviate xs =  foldr (\x acc -> (collectUpper x) ++ acc) [] . filter allCaps $ words xs
+abbreviate xs =  foldr (\x acc -> (collectUpper x) ++ acc) []
+    . map reduceIfAllCaps
+    $ words [if elem x "-_" then ' ' else x | x <- xs]
