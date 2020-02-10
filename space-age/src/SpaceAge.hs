54059003795212760fca106752368ffd6737fc8b
Dmitriy Dubson <d.dubson@gmail.com>
2020-02-10 17:55:52 -0500

    Space Age: iteration 2

--- space-age/src/SpaceAge.hs
+++ space-age/src/SpaceAge.hs
@@ -9,18 +9,19 @@ data Planet = Mercury
             | Uranus
             | Neptune
 
+orbitalOffset :: Planet -> Float
+orbitalOffset Earth = 1
+orbitalOffset Mercury = 0.2408467
+orbitalOffset Venus = 0.61519726
+orbitalOffset Mars = 1.8808158
+orbitalOffset Jupiter = 11.862615
+orbitalOffset Saturn = 29.447498
+orbitalOffset Uranus = 84.016846
+orbitalOffset Neptune = 164.79132
+
 ageOn :: Planet -> Float -> Float
-ageOn planet seconds = case planet of
-    Earth -> ageByOrbitalPeriodOffset $ 1
-    Mercury -> ageByOrbitalPeriodOffset $ 0.2408467
-    Venus -> ageByOrbitalPeriodOffset $ 0.61519726
-    Mars -> ageByOrbitalPeriodOffset $ 1.8808158
-    Jupiter -> ageByOrbitalPeriodOffset $ 11.862615
-    Saturn -> ageByOrbitalPeriodOffset $ 29.447498
-    Uranus -> ageByOrbitalPeriodOffset $ 84.016846
-    Neptune -> ageByOrbitalPeriodOffset $ 164.79132
-    _ -> 0.0
+ageOn planet seconds = ageByOffset . orbitalOffset $ planet
     where
         earthSecs = 31557600
-        ageByOrbitalPeriodOffset = (seconds /) . (*earthSecs)
+        ageByOffset = (seconds /) . (*earthSecs)
 
