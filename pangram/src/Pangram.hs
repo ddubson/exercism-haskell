dd102fcaa3808b83074cb23e9976a4a9c5c8c366
Dmitriy Dubson <d.dubson@gmail.com>
2020-03-16 13:06:18 -0400

    Pangram: 3rd iter

--- pangram/src/Pangram.hs
+++ pangram/src/Pangram.hs
@@ -4,7 +4,6 @@ import Data.Char (isAlpha, isAscii, toLower)
 import Data.List (intersect)
 
 isPangram :: String -> Bool
-isPangram text = (== ['a'..'z']) . (['a'..'z'] `intersect`). map (toLower) . filter onlyAsciiLetters $ text
+isPangram text = (==26) . length . (['a'..'z'] `intersect`). map (toLower) . filter onlyAsciiLetters $ text
     where
         onlyAsciiLetters = (\x -> isAlpha x && isAscii x)
-
