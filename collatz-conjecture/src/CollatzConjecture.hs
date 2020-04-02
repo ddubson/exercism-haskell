69e482468684cb3c42c317b40497570c3d7fbf1b
Dmitriy Dubson <d.dubson@gmail.com>
2020-04-02 11:50:23 -0400

    CollatzConjecture: 4th iter

--- collatz-conjecture/src/CollatzConjecture.hs
+++ collatz-conjecture/src/CollatzConjecture.hs
@@ -1,12 +1,12 @@
 module CollatzConjecture (collatz) where
 
 collatz :: Integer -> Maybe Integer
-collatz n = collatzK n 0
-        where
-            collatzK x calls
-                | x < 1 = Nothing
-                | x == 1 = Just calls
-                | otherwise = collatzK result (calls+1)
-                where
-                  result = if even x then fst (x `quotRem` 2) else (x * 3 + 1)
+collatz n | n < 1 = Nothing
+          | otherwise = collatzK n 0
+            where
+                collatzK x calls
+                    | x == 1 = Just calls
+                    | otherwise = collatzK result (calls+1)
+                    where
+                      result = if even x then (x `quot` 2) else (x * 3 + 1)
 
