8142cbf62eb0fb95cfefb84356674b1abe1559d3
Dmitriy Dubson <d.dubson@gmail.com>
2020-04-06 19:35:28 -0400

    RNA Transcription: 1st Iter

--- rna-transcription/src/DNA.hs
+++ rna-transcription/src/DNA.hs
@@ -1,11 +1,10 @@
 module DNA (toRNA) where
 
-import Data.Either (fromRight)
-
 toRNA :: String -> Either Char String
-toRNA "" = Right ""
-toRNA "C" = Right "G"
-toRNA "G" = Right "C"
-toRNA "T" = Right "A"
-toRNA "A" = Right "U"
-toRNA (x:xs) = (toRNA x:[]) ++ (toRNA $ xs)
+toRNA input = traverse (translate) input
+                  where
+                    translate 'C' = Right 'G'
+                    translate 'G' = Right 'C'
+                    translate 'T' = Right 'A'
+                    translate 'A' = Right 'U'
+                    translate otherChar = Left otherChar
